1b4256886adfbab204a41eacd913c9037401acac
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-02 13:09:57 -0800

    Move about page logic to PageController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('pages.about');
-});
+Route::get('/about', [PageController::class, 'about']);
