89308b0ed786c5a6d4a5fb374ead9d454e031e39
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-02 22:03:14 -0800

    add contact page template

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,3 +1,25 @@
+@extends('layouts.master')
+
+@section('title', 'Contact')
+
+@section('content')
+
+<h1>Contact Us</h1>
+<p>have an exciting project? Let's work together!</p>`
+
 <div>
-    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    <form action="">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" required></textarea>
+
+        <button type="submit">Send Message</button>
+    </form>
 </div>
+
+@endsection
